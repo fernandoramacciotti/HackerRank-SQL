bbe4876e2355afd71d6e1d6571241b1e83b86eb5
fernandoramacciotti <fernandoramacciotti@gmail.com>
2017-09-15 01:27:21 -0300

    upload

---
+++ Advanced Select/The_PADS.sql
@@ -0,0 +1,29 @@
+(
+SELECT
+   CONCAT(a.name, '(', UPPER(LEFT(a.occup, 1)), ')') as first
+FROM
+    (SELECT
+        Name as name,
+        Occupation as occup
+    FROM
+        OCCUPATIONS
+    ORDER BY
+        name) a
+)
+UNION ALL
+(
+SELECT
+    CONCAT('There are a total of ', b.freq, ' ', b.occup, 's.') as second
+FROM
+    (SELECT
+        LOWER(Occupation) AS occup,
+        count(Occupation) AS freq
+    FROM
+        OCCUPATIONS
+    GROUP BY
+        Occupation
+    ORDER BY
+        freq, occup) b
+)
+ORDER BY
+    first
